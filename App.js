2a638bd992181a6509b841ef500d5f19ffa4655f
vunamweb <vukynamkhtn@gmail.com>
2022-09-17 11:14:10 +0700

    setup environment

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Component} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createAppContainer } from 'react-navigation';
+
+import Function from './app/function/function';
+
+const AppNavigator = Function.initNavigarion();
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return (
+       <AppContainer/>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/function/function.js
@@ -0,0 +1,26 @@
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from '../../app/navigation/Home';
+import ShowNear from '../../app/navigation/Home';
+
+class Functions {
+    initNavigarion = () => {
+        return createStackNavigator(
+            {
+            Home: {
+              screen: Home,
+              navigationOptions: {
+                header: null,
+            },
+            },
+            ShowNear: {
+              screen: ShowNear,
+            },
+            
+          }
+          );
+    }
+}
+
+const functions = new Functions();
+export default functions;
